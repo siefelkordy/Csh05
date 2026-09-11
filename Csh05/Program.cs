dbb1137c6230b0f777ae35009817f74d0b4317d1
siefelkordy <seifemadelkordy@gmail.com>
2026-09-11 15:31:59 +0300

    Csh04 1st answer

--- Csh05/Program.cs
+++ Csh05/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //C#Basics-04 Assignment 
+            #region
+            //1st answer
+            double[] prices = new double[3];
+            prices[0] = 25.5;
+            prices[1] = 40;
+            prices[2] = 33.75;
+            Console.WriteLine(prices[1]);
+
+            #endregion
+
+
         }
     }
 }
